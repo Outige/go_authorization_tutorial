8cbee3a2d9e8c84933d7e2f9c633478b28849a67
Tieg <tiegosullivanpsnl@gmail.com>
2020-05-15 08:10:01 +0200

    COMPLETE

--- client/main.go
+++ client/main.go
@@ -2,21 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
+
 	"github.com/dgrijalva/jwt-go" // go get -u github.com/dgrijalva/jwt-go
 )
 
+/* signing key */
+// $ set MY_JWT_TOKEN=mysupersecretphrase
+// var mySigningKey = os.Get("MY_JWT_TOKEN")	//? best practices for passwords
 var mySigningKey = []byte("mysupersecretphrase")
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	validToken, err := generateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	// fmt.Fprintf(w, validToken)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:8000/", nil)
+	req.Header.Set("Token", validToken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "Error %s", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, string(body))
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
 
 func generateJWT() (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
-	
+
 	claims := token.Claims.(jwt.MapClaims)
-	
+
 	claims["authorized"] = true
 	claims["user"] = "Elliot Forbes"
 	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
-	
+
 	tokenString, err := token.SignedString(mySigningKey)
 
 	if err != nil {
@@ -27,11 +62,7 @@ func generateJWT() (string, error) {
 }
 
 func main() {
-	tokenString, err := generateJWT()
+	fmt.Print("simple client...\n\n")
 
-	if err != nil {
-		fmt.Println("error generating token string")
-		return
-	}
-	fmt.Println(tokenString)
+	handleRequests()
 }
